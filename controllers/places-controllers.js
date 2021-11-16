1c50ac2746c155f9bda5393c416975cfd4ed45a1
Nafiul <nafiulhaque22@gmail.com>
2021-11-16 21:03:48 +0600

    deleting place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -3,7 +3,7 @@ const { v4: uuid } = require('uuid');
 const HttpError = require('../models/http-error');
 
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
     {
         id: 'p1',
         title: 'Empire state bouilding',
@@ -33,17 +33,17 @@ const getPlaceById = (req, res, next) => {
 };
 
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.uid;
 
-    const place = DUMMY_PLACES.find(p => {
+    const places = DUMMY_PLACES.filter(p => {
         return p.creator === userId;
     });
-    if (!place) {
+    if (!places || places.length === 0) {
 
-        return next(new Error('Could not find a place for the provided user id?', 404));
+        return next(new Error('Could not find  places for the provided user id?', 404));
     }
-    res.json({ place });
+    res.json({ places });
 };
 
 
@@ -80,12 +80,14 @@ const updatePlace = (req, res, next) => {
 
 
 const deletePlace = (req, res, next) => {
-
+    const placeId = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+    res.status(200).json({ message: 'Deleted place.' })
 };
 
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
