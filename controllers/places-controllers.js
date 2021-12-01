f4249ad599a31ae63b43d592269629ba422ad78c
Nafiul <nafiulhaque22@gmail.com>
2021-12-01 19:48:30 +0600

    Deleting places with relation db

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -190,7 +190,7 @@ const deletePlace = async (req, res, next) => {
 
     let place;
     try {
-        place = await Place.findById(placeId);
+        place = await (await Place.findById(placeId)).populate('creator');
     } catch (err) {
         const error = new HttpError(
             'Something went wrong , could not delete place',
@@ -199,8 +199,22 @@ const deletePlace = async (req, res, next) => {
         return next(error);
     }
 
+    if (!place) {
+        const error = new HttpError(
+            'could not find place for this id.',
+            404
+        );
+        return next(error);
+    }
+
     try {
-        await place.remove();
+        const sess = await mongoose.startSession();
+        sess.startTransaction();
+        await place.remove({ session: sess });
+        place.creator.places.pull(place);
+        await place.creator.save({ session: sess });
+        await sess.commitTransaction();
+
     } catch (err) {
         const error = new HttpError(
             'Something went wrong , could not delete place',
