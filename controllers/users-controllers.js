62c96656a168a9c3f6b0f8111ba90ac461e5ee4c
Nafiul <nafiulhaque22@gmail.com>
2021-12-09 20:06:03 +0600

    generating tokens (JET) on the backend

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -82,7 +83,25 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up  failed',
+            500
+        )
+        return next(error);
+    }
+
+
+    res
+        .status(201)
+        .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 
@@ -101,7 +120,43 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in',
+            401
+        );
+        return next(error);
+    }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+
+    } catch (err) {
+        const error = new HttpError(
+            'Could not log you in , please check your credentials and try again',
+            500
+        );
+        return next(error);
+    }
+
+
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Loging  failed',
+            500
+        )
+        return next(error);
+    }
+
+    if (!isValidPassword) {
         const error = new HttpError(
             'Invalid credentials, could not log you in',
             401
@@ -111,8 +166,9 @@ const login = async (req, res, next) => {
 
 
     res.json({
-        message: 'Logged in!',
-        user: existingUser.toObject({ getters: true })
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token
     });
 };
 
